d9965ef41097837404929b0f89af921e8a2724a2
marcuslangford <marcus.d.langford96@gmail.com>
2022-03-01 10:53:01 +0000

    Update ParkingTest.java

--- ParkingTest.java
+++ ParkingTest.java
@@ -17,17 +17,24 @@ public class ParkingTest {
         //  Navigate from the home page to the find parking page
         home.enterLocation();
         home.changeFromDate();
+
+        //  The button is not clickable, until the Location field is full,
+        //  so added a wait here to ensure that the button is clickable.
+        //  Note:   I tried to use 'isClickable' with an explicit wait
+        //          but was having trouble there.
+        Thread.sleep(2000);
         home.showParking();
 
         //  Wait for parking page to load before beginning parking page methods
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //  Start by verifying that the modal element is not present, change the date to >28 days, re-search
+        //  I use this just to check my method of verifying whether the modal is showing works
         parking.verifyModal();
         parking.openDateTo();
         parking.search();
 
-        //  Wait for enough time so the modal shows, verify the element is present
+        //  Wait for enough time so the modal shows, verify the element is present in the console output
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         parking.verifyModal();
 
